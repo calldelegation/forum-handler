9d83e60423eb6ffe30cc790fc2d4fa91e5855e69
cold-briu <andresf.xyz@gmail.com>
2023-07-24 17:06:50 -0500

    feat: added simple test

--- api/scripts/post_edited.test.sh
+++ api/scripts/post_edited.test.sh
@@ -1,22 +1,9 @@
 #!/bin/bash
+source test_endpoint.sh
 
-# Define the function test_endpoint
-test_endpoint() {
-  local data_path=$1
-  local endpoint_url="http://localhost:3000/api/handler"
-  local event_type="post"
-  local event="post_edited"
-  
-  # Print the request body
-  echo "Request Body:"
-  cat "$data_path"
-  
-  # Send POST request with headers and request body, and print the response
-  local response=$(curl -X POST -H "Content-Type: application/json" -H "x-discourse-event-type: $event_type" -H "X-Discourse-Event: $event" --data @"$data_path" "$endpoint_url")
-  echo "Response:"
-  echo "$response"
-}
+sample_data_path="../example-events/post-edited.json"
+forum_event_type="post"
+forum_event_case="post_edited" 
 
-# Call the test_endpoint function with the provided argument
-sample_data_path="../example-events/edited-post.json"
-test_endpoint "$sample_data_path"
+test_endpoint "$sample_data_path" "$forum_event_type" "$forum_event_case"
+
--- api/scripts/staff_reply_created.test.sh
+++ api/scripts/staff_reply_created.test.sh
@@ -1,7 +1,9 @@
+
+
 #!/bin/bash
 source test_endpoint.sh
 
-sample_data_path="../example-events/post-created.json"
+sample_data_path="../example-events/staff-reply-created.json"
 forum_event_type="post"
 forum_event_case="post_created" 
 
